d48537015911224e8e72b0ed835fd8dfca5858e9
ngactive99 <ngactive99@gmail.com>
2022-04-23 21:14:59 +0530

    update lifecycle method complete

--- src/components/phone.js
+++ src/components/phone.js
@@ -1,22 +1,31 @@
 import React from "react";
 
 class Phone extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state={phone : "Iphone7"}
     }
     shouldComponentUpdate() {
         return true // this defines that a change in the value of a state should or should not re-render the page
     }
-    upgrade = () => {
-        this.setState({phone: "Iphone 10"});
-        console.log(this.phone);
+   componentDidMount() {
+       setTimeout(() => {
+           this.setState({phone: "Iphone 10"})
+       }, 3000)
+   }
+    getSnapshotBeforeUpdate( prevState) {
+        document.getElementById("heading1").innerHTML= "my old phone was " + prevState.phone;
+    }
+    componentDidUpdate() {
+        document.getElementById("heading2").innerHTML = "the new phone is " + this.state.phone
     }
     render() {
         return (
             <div>
                 <h1>My current phone is a {this.state.phone}</h1>
-                <button type="button" onClick={this.upgrade}>Upgrade Phone</button>
+                <h2 id="heading1"></h2>
+                <h2 id="heading2"></h2>
+                {/* <button type="button" onClick={this.upgrade}>Upgrade Phone</button> */}
             </div>
         )
     }
