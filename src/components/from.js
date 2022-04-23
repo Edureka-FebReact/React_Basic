b6f91d42c6cde9da6d981e62c97c8c5f9e7cc92b
ngactive99 <ngactive99@gmail.com>
2022-04-23 22:49:30 +0530

    styling

--- src/components/from.js
+++ src/components/from.js
@@ -3,15 +3,20 @@ import { React, Component } from "react";
 class Form extends Component {
   constructor() {
     super();
-    this.state = { name: "", age: null };
+    this.state = {
+      name: "",
+      age: null,
+      desc: "Google LLC is an American multinational technology company that focuses on artificial intelligence, search engine, online advertising, cloud computing, computer software, quantum computing, e-commerce, and consumer electronics.",
+      gender: "Select",
+    };
   }
   onChangeVal = (event) => {
     let nam = event.target.name;
     let val = event.target.value;
-    if(nam ==='age') {
-        if(!Number(val)){
-            alert("Your age must be a numeric value")
-        }
+    if (nam === "age") {
+      if (!Number(val)) {
+        alert("Your age must be a numeric value");
+      }
     }
     this.setState({ [nam]: val });
     console.log(this.state.name);
@@ -23,11 +28,18 @@ class Form extends Component {
   render() {
     return (
       <form>
-        <h1>Register form</h1>
+        <h1 >Register form</h1>
         <input type="text" name="name" onChange={this.onChangeVal} /> Name
         <input type="text" name="age" onChange={this.onChangeVal} /> age
         <br />
         <h2>your age is {this.state.age}</h2>
+        <br />
+        <textarea value={this.state.desc}></textarea>
+        <br />
+        <select value={this.state.gender}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
         {/* <input type="text" /> Designation */}
       </form>
     );
